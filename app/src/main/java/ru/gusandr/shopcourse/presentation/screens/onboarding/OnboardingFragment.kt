c2812ba65eb7f28549681fd8e59c9893f42d553c
Rxlol <chailoshkarev@mail.ru>
2025-10-25 18:37:25 +0300

    refactoring onboarding

--- app/src/main/java/ru/gusandr/shopcourse/presentation/screens/onboarding/OnboardingFragment.kt
+++ app/src/main/java/ru/gusandr/shopcourse/presentation/screens/onboarding/OnboardingFragment.kt
@@ -1,8 +1,33 @@
 package ru.gusandr.shopcourse.presentation.screens.onboarding
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import ru.gusandr.shopcourse.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment() {
 
-    //private var _binding: FragmentLoginBinding? = null
+    private var _binding: FragmentOnboardingBinding? = null
+
+    private val binding
+        get() = _binding!!
+
+    private val viewModel: OnboardingViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
